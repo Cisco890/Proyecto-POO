67052048d36bc02735def9e9759b2ab9d1e0e2fb
dannyrmrz <134820840+dannyrmrz@users.noreply.github.com>
2023-10-05 17:02:51 -0600

    Commit 2 Daniela Ramírez
    Creación de la clase "Residente"  en el que se define el atributo "placa" la cual nos servirá para  verificar que el cliente sea un residente en función a nuestra base de datos (un archivo csv)

---
+++ Residente.java
@@ -0,0 +1,11 @@
+class Residente {
+    private String placa;
+
+    public Residente(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+}
