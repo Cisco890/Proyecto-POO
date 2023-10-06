74e5be49b6900f85e2b80c96c1c1c7441981b162
Anthony Lou Schwank <123795140+anthonylouschwank@users.noreply.github.com>
2023-10-06 07:16:39 -0600

    Commit 1
    Terminamos la clase residente

--- Residente.java
+++ Residente.java
@@ -1,11 +1,36 @@
 public class Residente {
+    private String nombre;
     private String placa;
-
-    public Residente(String placa) {
+    private String Tipovehiculo;
+    private String color;
+    private String marca;
+    
+    public Residente(String nombre, String Tipovehiculo, String placa, String color, String marca) {
+        this.nombre = nombre;
         this.placa = placa;
+        this.Tipovehiculo = Tipovehiculo;
+        this.color = color;
+        this.marca = marca;
+    }
+
+    public String getnombre(){
+        return nombre;
     }
 
     public String getPlaca() {
         return placa;
     }// se finalizan los getters del objeto ClienteRegular
+
+    public String getTipovehiculo() {
+        return Tipovehiculo;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+    
+    public String getmarca() {
+        return marca;
+    }
+
 }
