ab910d8c4c7eec427047cf798eb925d534a6cdbe
Cisco <123789016+Cisco890@users.noreply.github.com>
2023-11-09 01:15:07 -0600

    Add files via upload
    fin de comentar todas las clases

--- Movimiento.java
+++ Movimiento.java
@@ -1,11 +1,19 @@
+/** Juan Francisco Martínez 23617, Andrés Mazariegos 21749,  Daniela Ramirez 23053,  Antony Lou Schanwk 23410
+
+  * Movimiento
+  * @param placa,horaEntrada,horaSalida
+  * @throws Es la clase padre del programa, se encarga de ser extendida por las otras dos
+
+  */
 import java.time.LocalDateTime;
+//importar la libreria de time.LocalDateTime
 
 public class Movimiento {
     protected String placa;
     protected LocalDateTime horaEntrada;
     protected LocalDateTime horaSalida;
 
-    public Movimiento(String placa, LocalDateTime horaEntrada, LocalDateTime horaSalida) {
+    public Movimiento(String placa, LocalDateTime horaEntrada, LocalDateTime horaSalida) {//constructor de Movimiento
         this.placa = placa;
         this.horaEntrada = horaEntrada;
         this.horaSalida = horaSalida;
@@ -26,4 +34,5 @@ public class Movimiento {
     public void setHoraSalida(LocalDateTime horaSalida) {
         this.horaSalida = horaSalida;
     }
+    //Stters y getters de la clase Movimientos
 }
